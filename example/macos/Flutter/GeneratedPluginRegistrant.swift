d6a18972cea4b192787ea3fddddc0d378059d8cb
Sarbagya Dhaubanjar <sarbagyastha@gmail.com>
2025-03-15 18:05:09 +0545

    :apple: macos support added for example

---
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import file_encrypter
+import path_provider_foundation
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FileEncrypterPlugin.register(with: registry.registrar(forPlugin: "FileEncrypterPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+}
