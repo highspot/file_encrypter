8e25bc2f0927449919615c3494f3dd3d1b43101d
Sarbagya Dhaubanjar <sarbagyastha@gmail.com>
2025-03-19 09:11:28 +0545

    :zap: faster encryption/decryption

--- darwin/file_encrypter/Sources/file_encrypter/FileEncrypterPlugin.swift
+++ darwin/file_encrypter/Sources/file_encrypter/FileEncrypterPlugin.swift
@@ -14,8 +14,8 @@ import FlutterMacOS
 #endif
 
 public class FileEncrypterPlugin: NSObject, FlutterPlugin, FileEncrypterApi {
-    let bufferSize = 65536
-    
+    let bufferSize = 131072
+
     public static func register(with registrar: FlutterPluginRegistrar) {
         let instance = FileEncrypterPlugin()
 #if os(iOS)
@@ -27,7 +27,7 @@ public class FileEncrypterPlugin: NSObject, FlutterPlugin, FileEncrypterApi {
     }
     
     internal func encrypt(inFileName: String, outFileName: String, completion: @escaping (Result<String, Error>) -> Void) {
-        DispatchQueue.global(qos: .background).async {
+        DispatchQueue.global(qos: .userInitiated).async {
             guard let fileIn = InputStream(fileAtPath: inFileName),
                   let fileOut = OutputStream(toFileAtPath: outFileName, append: false) else {
                 completion(.failure(PigeonError(code: "INIT_FAILED", message: "Failed to initialize file streams.", details: "")))
@@ -42,10 +42,10 @@ public class FileEncrypterPlugin: NSObject, FlutterPlugin, FileEncrypterApi {
                 completion(.failure(PigeonError(code: "INVALID_KEY", message: "Failed to generate encryption key.", details: "")))
                 return
             }
-            
+
             let key = [UInt8](secretKeyData)
             fileOut.write(iv, maxLength: iv.count)
-            
+
             do {
                 let encryptor = try ChunkCryptor(encrypt: true, key: key, iv: iv)
                 self.crypt(action: encryptor, from: fileIn, to: fileOut)
@@ -59,32 +59,32 @@ public class FileEncrypterPlugin: NSObject, FlutterPlugin, FileEncrypterApi {
     }
     
     internal func decrypt(key: String, inFileName: String, outFileName: String, completion: @escaping (Result<Void, Error>) -> Void) {
-        DispatchQueue.global(qos: .background).async {
+        DispatchQueue.global(qos: .userInitiated).async {
             guard let fileIn = InputStream(fileAtPath: inFileName),
                   let fileOut = OutputStream(toFileAtPath: outFileName, append: false) else {
                 completion(.failure(PigeonError(code: "INIT_FAILED", message: "Failed to initialize file streams.", details: "")))
                 return
             }
-            
+
             fileIn.open()
             fileOut.open()
-            
+
             guard let secretKeyData = Data(base64Encoded: key) else {
                 completion(.failure(PigeonError(code: "INVALID_KEY", message: "Invalid Base64 Key.", details: "")))
                 return
             }
-            
-            var iv = [UInt8](repeating: 0, count: 16)
-            let bytesRead = fileIn.read(&iv, maxLength: iv.count)
-            guard bytesRead == iv.count else {
+
+            var iv = Data(count: 16)
+            let bytesRead = iv.withUnsafeMutableBytes { fileIn.read($0.baseAddress!.assumingMemoryBound(to: UInt8.self), maxLength: 16) }
+            guard bytesRead == 16 else {
                 completion(.failure(PigeonError(code: "IV_READ_FAILED", message: "Failed to read IV from encrypted file.", details: "")))
                 return
             }
-            
+
             let secretKey = [UInt8](secretKeyData)
-            
+
             do {
-                let decryptor = try ChunkCryptor(encrypt: false, key: secretKey, iv: iv)
+                let decryptor = try ChunkCryptor(encrypt: false, key: secretKey, iv: [UInt8](iv))
                 self.crypt(action: decryptor, from: fileIn, to: fileOut)
                 fileOut.close()
                 fileIn.close()
@@ -94,7 +94,7 @@ public class FileEncrypterPlugin: NSObject, FlutterPlugin, FileEncrypterApi {
             }
         }
     }
-    
+
     private func generateRandomKey(length: Int) -> Data? {
         var key = Data(count: length)
         let result = key.withUnsafeMutableBytes { SecRandomCopyBytes(kSecRandomDefault, length, $0.baseAddress!) }
@@ -102,37 +102,42 @@ public class FileEncrypterPlugin: NSObject, FlutterPlugin, FileEncrypterApi {
     }
     
     @discardableResult private func crypt(action sc: ChunkCryptor, from inputStream: InputStream, to outputStream: OutputStream) -> (bytesRead: Int, bytesWritten: Int) {
-        var inputBuffer = [UInt8](repeating: 0, count: bufferSize)
-        var outputBuffer = [UInt8](repeating: 0, count: bufferSize)
+        let inputBuffer = UnsafeMutablePointer<UInt8>.allocate(capacity: bufferSize)
+        let outputBuffer = UnsafeMutablePointer<UInt8>.allocate(capacity: bufferSize)
         
         var totalBytesRead = 0
         var totalBytesWritten = 0
-        
+
+        defer {
+            inputBuffer.deallocate()
+            outputBuffer.deallocate()
+        }
+
         while inputStream.hasBytesAvailable {
-            let bytesRead = inputStream.read(&inputBuffer, maxLength: bufferSize)
+            let bytesRead = inputStream.read(inputBuffer, maxLength: bufferSize)
             guard bytesRead > 0 else { break }
-            
+
             totalBytesRead += bytesRead
             var cryptedBytes: Int = 0
-            
-            let status = sc.update(bufferIn: inputBuffer, byteCountIn: bytesRead, bufferOut: &outputBuffer, byteCapacityOut: bufferSize, byteCountOut: &cryptedBytes)
+
+            let status = sc.update(bufferIn: inputBuffer, byteCountIn: bytesRead, bufferOut: outputBuffer, byteCapacityOut: bufferSize, byteCountOut: &cryptedBytes)
             if status != kCCSuccess {
                 return (totalBytesRead, totalBytesWritten)
             }
-            
+
             if cryptedBytes > 0 {
                 let bytesWritten = outputStream.write(outputBuffer, maxLength: cryptedBytes)
                 totalBytesWritten += bytesWritten
             }
         }
-        
+
         var finalBytes: Int = 0
-        let status = sc.final(bufferOut: &outputBuffer, byteCapacityOut: bufferSize, byteCountOut: &finalBytes)
+        let status = sc.final(bufferOut: outputBuffer, byteCapacityOut: bufferSize, byteCountOut: &finalBytes)
         if status == kCCSuccess, finalBytes > 0 {
             let bytesWritten = outputStream.write(outputBuffer, maxLength: finalBytes)
             totalBytesWritten += bytesWritten
         }
-        
+
         return (totalBytesRead, totalBytesWritten)
     }
 }
@@ -141,7 +146,7 @@ extension String {
     func fromBase64() -> Data? {
         return Data(base64Encoded: self)
     }
-    
+
     func toBase64() -> String {
         return Data(utf8).base64EncodedString()
     }
